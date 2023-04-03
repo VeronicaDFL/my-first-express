03ca1f3b1db3eb0f6e39c48d4723cb80316322b7
Veronica De Felice <verodefelic@gmail.com>
2023-04-03 11:56:11 -0400

    built simple api

---
+++ index.js
@@ -0,0 +1,25 @@
+
+import express from 'express'
+
+const PORT = 3000
+
+const app = express()
+
+// LIST OF ALLOWED REQUESTS:
+
+app.get("/hello", (req, res) => {
+
+    res.send("Hello there!")
+})
+
+app.get("/test", (req, res) =>{
+res.send("It's working!")
+})
+
+
+
+
+app.listen(PORT, () =>{
+
+    console.log(`Listening on http://localhost:${PORT}...`)
+})
